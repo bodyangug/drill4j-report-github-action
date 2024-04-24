7e2f6db4fcf3e2501f0fcc7e1982bfb502ffcc74
Bohdan <bohdan_asieiev@epam.com>
2024-04-24 19:26:14 +0300

    feat: get env vars

--- src/main/kotlin/com/epam/drill/github/action/ExecuteMakePrComments.kt
+++ src/main/kotlin/com/epam/drill/github/action/ExecuteMakePrComments.kt
@@ -1,12 +1,11 @@
 package com.epam.drill.github.action
 
-import com.epam.drill.github.action.logger.debug
 
 fun main() {
-    val whoToGreet = System.getenv("INPUT_WHO-TO-GREET")
-    debug("WhoToGreet = $whoToGreet")
-    val eventPath = System.getenv("GITHUB_EVENT_PATH")
-    debug("eventPath = $eventPath")
+    val whoToGreet = System.getenv("INPUT_WHO-TO-GREET") ?: "empty-who-to-greet"
+    println("whoToGreet = $whoToGreet")
+    val eventPath = System.getenv("GITHUB_EVENT_PATH") ?: "empty-event"
+    println("eventPath = $eventPath")
     println("Comments were made!")
 //    val commentService = CommonPrCommentService()
 //    val result = commentService.makePrComments(args, HttpUrl.get(URL_GITHUB))
