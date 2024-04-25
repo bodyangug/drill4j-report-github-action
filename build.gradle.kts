f110f971d1564520517c0153681abe5672881251
Bohdan <bohdan_asieiev@epam.com>
2024-04-25 17:35:18 +0300

    feat: set-up versions for kotlin 1.5.31, add logging dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,13 @@ tasks {
 }
 
 dependencies {
-    implementation("com.squareup.moshi:moshi:1.9.3")
-    implementation("com.squareup.moshi:moshi-kotlin:1.9.3")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    //INFO: All dependencies for Kotlin 1.5.31
+    implementation("com.squareup.moshi:moshi:1.13.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    //Logging:
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+    implementation("ch.qos.logback:logback-classic:1.2.6")
 }
